4c485be4f17a3ffe3861715ee58ccbcc5e6f7bcc
Daniel <daniel.jarai@ucuenca.edu.ec>
2024-09-27 23:36:46 -0500

    Se actualizó el among us y el fondo.

--- sketch.js
+++ sketch.js
@@ -21,6 +21,30 @@
 //   rect(mouseX, mouseY, 100, 100);
 // }
 
+//AMONG US
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   background(0);
+//   rectMode(CENTER);
+// }
+
+// function draw() {
+//   background(0);
+
+//   noStroke();
+//   fill(217, 22, 86);
+//   rect(mouseX - 45, mouseY + 6, 40, 65, 20, 0, 0, 20);
+//   rect(mouseX, mouseY, 70, 100, 40, 40, 0, 0);
+//   fill(245, 245, 247);
+//   rect(mouseX + 20, mouseY - 10, 45, 25, 40);
+//   fill(217, 22, 86);
+//   rect(mouseX + 22.5, mouseY + 60, 25, 40);
+//   rect(mouseX - 22.5, mouseY + 60, 25, 40);
+// }
+
+//POCHITA
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
@@ -28,15 +52,19 @@ function setup() {
 }
 
 function draw() {
-  background(0);
+  background(random(10, 200), random(0, 100), 255, 60);
 
   noStroke();
-  fill(217, 22, 86);
-  rect(mouseX - 45, mouseY + 6, 40, 65, 20, 0, 0, 20);
-  rect(mouseX, mouseY, 70, 100, 40, 40, 0, 0);
+  fill(200, 22, 86);
+  rect(mouseX - 45, mouseY + 6, 40, 65, 20, 0, 0, 20); //mochila
+  rect(mouseX, mouseY, 70, 100, 40, 40, 0, 0); //cuerpo
+  fill(255, 22, 86);
+  rect(mouseX, mouseY - 10, 70, 80, 40, 40, 0, 100); //sombra
   fill(245, 245, 247);
-  rect(mouseX + 20, mouseY - 10, 45, 25, 40);
-  fill(217, 22, 86);
-  rect(mouseX + 22.5, mouseY + 60, 25, 40);
-  rect(mouseX - 22.5, mouseY + 60, 25, 40);
+  rect(mouseX + 20, mouseY - 10, 45, 25, 40); //lente
+  fill(104, 109, 118);
+  rect(mouseX + 25, mouseY - 13, 20, 5, 40); //Reflejo
+  fill(200, 22, 86);
+  rect(mouseX + 22.5, mouseY + 60, 25, 40); // P.1
+  rect(mouseX - 22.5, mouseY + 60, 25, 40); //P.2
 }
